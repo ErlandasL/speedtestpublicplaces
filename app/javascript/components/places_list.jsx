261bbe29bdb5084a93f5bb52aa5b418812293bec
ErlandasL <erlandaslisauskas@gmail.com>
2023-01-23 09:01:59 +0200

    Add: render list in rails and react

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
+function PlacesList() {
+  const [loading, setLoading] = useState(true)
+  const [loadedPlaces, setLoadedPlaces] = useState([])
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+}
 class PlacesList extends React.Component {
   render() {
     return (
